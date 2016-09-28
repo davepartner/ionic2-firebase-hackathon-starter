1991882c27716d392eb40de83e5b6e15c0c422f9
daveozoalor <daveozoalor@gmail.com>
2016-09-28 09:28:38 +0100

    First upload

---
+++ app/pages/login/login.ts
@@ -0,0 +1,181 @@
+import { Component } from '@angular/core';
+import { NavController, ModalController, LoadingController, AlertController, ToastController} from 'ionic-angular';
+import { RegisterPage } from '../register/register';
+import { HomePage } from '../home/home';
+import { UsersService } from '../../providers/users-service/users-service';
+
+/*
+  Generated class for the LoginPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/login/login.html',
+  providers:[UsersService]
+})
+export class LoginPage {
+
+public emailField: any; 
+public passwordField: any; 
+private users = [];
+private usersList : any;
+
+  constructor(private alertCtrl: AlertController , private loadingCtrl: LoadingController, private navCtrl: NavController, private modalCtrl: ModalController, private usersService: UsersService, private toastCtrl: ToastController) {
+				
+				this.emailField = "";
+				this.passwordField = "";
+  				this.listOurUsers();
+  
+  }
+  
+  
+  
+  
+  signUserUp(){
+  	
+  	this.usersService.signUpUser(this.emailField, this.passwordField).then(authData => {
+  		//successful
+  		this.navCtrl.setRoot(HomePage);
+  	}, error => {
+  		//alert("error logging in: "+ error.message);
+  		
+  		
+  	});
+  	
+  	
+  	let loader = this.loadingCtrl.create({
+  		dismissOnPageChange: true,
+  	});
+  	
+  	loader.present();
+  	
+  }
+
+listOurUsers(){
+	this.usersService.loadUser(10)
+	.then(data => {
+		this.usersList = data;
+	})
+}
+
+
+//login
+  submitLogin(){
+  	
+  		alert(this.passwordField);
+  	this.usersService.loginUser(this.emailField, this.passwordField).then(authData => {
+  		//successful
+  		this.navCtrl.setRoot(HomePage);
+  		
+  	}, error => {
+  		//alert("error logging in: "+ error.message);
+  		let alert = this.alertCtrl.create({
+	      title: 'Error loggin in',
+	      subTitle: error.message,
+	      buttons: ['OK']
+	    });
+	    alert.present();
+  	});
+  	
+  	
+  	let loader = this.loadingCtrl.create({
+  		dismissOnPageChange: true,
+  	});
+  	
+  	loader.present();
+  	
+  	
+  	
+    }
+ 
+  
+  submitRegister(){
+  	 
+  	 let registerModal = this.modalCtrl.create(RegisterPage);
+  	 registerModal.present();
+  	 
+  }
+  
+
+showForgotPassword(){
+	
+	//
+	
+	 let prompt = this.alertCtrl.create({
+      title: 'Enter Your Email',
+      message: "A new password will be sent to your email",
+      inputs: [
+        {
+          name: 'recoverEmail',
+          placeholder: 'you@example.com'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Submit',
+          handler: data => {
+           
+            
+            //add preloader
+            let loading = this.loadingCtrl.create({
+				dismissOnPageChange: true,
+				content: 'Reseting your password..'
+			});
+			 loading.present();
+             //call usersservice
+            this.usersService.forgotPasswordUser(data.recoverEmail).then(() => {
+            	   //add toast
+            	     loading.dismiss().then(() => {
+            	     	//show pop up
+            	     		let alert = this.alertCtrl.create({
+					      title: 'Check your email',
+					      subTitle: 'Password reset successful',
+					      buttons: ['OK']
+					    });
+					    alert.present();
+            	     })
+            	
+            	}, error => {
+            		//show pop up
+            		loading.dismiss().then(() => {
+				  		let alert = this.alertCtrl.create({
+					      title: 'Error resetting password',
+					      subTitle: error.message,
+					      buttons: ['OK']
+					    });
+					    alert.present();
+					 })
+ 
+	    
+            	});
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+	
+	
+	googleSignIn(){
+		
+		this.usersService.googleSignInUser().then(()=>{
+			//success, redirect
+			let toast = this.toastCtrl.create({
+		      message: 'User account created successfully...',
+		      duration: 3000
+		    });
+		    toast.present();
+		
+		});	
+		  
+	}
+
+
+}
---
+++ app/pages/users-detail/users-detail.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { UsersService } from '../../providers/users-service/users-service';
+import { LoginPage } from '../login/login';
+import * as firebase from 'firebase';
+/*
+  Generated class for the UsersDetailPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/users-detail/users-detail.html',
+  providers:[UsersService]
+})
+
+
+
+export class UsersDetailPage {
+private userPhotoUrl: any;
+private userDislplayName: any;
+
+
+  constructor(private navCtrl: NavController, private usersService: UsersService) {
+
+var myUserId = firebase.auth().currentUser.uid; //current user id
+
+this.displayUser(myUserId);
+
+
+
+  }
+
+displayUser(theUserId){
+	
+	var that = this;
+	
+	this.usersService.viewUser(theUserId).then(snapshot => {
+	
+		 //get user photo
+		that.userPhotoUrl = snapshot.val().photo; //get user photo
+	   that.userDislplayName= snapshot.val().job || "Unemployed Due to recession"; 
+	})
+}
+
+logUserOut(){
+	//call user service
+	this.usersService.logoutUser().then(() => {
+		this.navCtrl.setRoot(LoginPage);
+	});
+}
+}
