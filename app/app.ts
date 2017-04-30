d2968d697c033fa92c752f38fb33f768bc63da40
Dave Partner <daveozoalor@gmail.com>
2017-04-30 15:17:05 +0100

    Fixed blank page error

--- app/app.ts
+++ app/app.ts
@@ -26,17 +26,17 @@ export class MyApp {
   firebase.initializeApp(config);
   	
   	//check logged in status
-  	firebase.auth().onAuthStateChanged((user) => {
-		
-		if(user){
-			this.rootPage = TabsPage;
-		}else {
-			//this.rootPage = LoginPage;
-			this.rootPage = LoginPage;
-		}
-		
-	});
-  	
+
+  var user = firebase.auth().currentUser; //this is what to change
+
+        if (user) {
+            // User is signed in.
+            this.rootPage = TabsPage;
+        } else {
+            // No user is signed in.
+            this.rootPage = LoginPage;
+        }
+
     
 
     platform.ready().then(() => {
